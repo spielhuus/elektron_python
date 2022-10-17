fd5a2489d28804ce6056fd926891984c9d083b7d
spielhuus <spielhuus@gmail.com>
2022-10-17 20:11:07 +0200

    debug print

--- src/lib.rs
+++ src/lib.rs
@@ -142,6 +142,7 @@ impl Draw {
             let num: u32 = rng.gen();
             let filename =
                 String::new() + temp_dir().to_str().unwrap() + "/" + &num.to_string() + "." + imagetype; */
+            println!("output plot to buffer");
             plot::plot_schema(&self.schema, None, scale, border, theme.as_str(), netlist, Some(imagetype)).unwrap();
             
             /* let mut f = File::open(&filename).expect("no file found");
